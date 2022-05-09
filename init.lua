b2d093e694bdfc6a27f90a96e979aadc97983319
MarcoGhi <marcoghigo98@gmail.com>
2022-05-09 12:26:52 +0000

    Added flutter support

--- init.lua
+++ init.lua
@@ -47,7 +47,12 @@ vim.keymap.set('n', '<C-p>', ':NERDTreeFocus<CR>')
 vim.keymap.set('n', '<C-b>', ':NERDTreeToggle<CR>')
 vim.keymap.set('n', '<C-f>', ':NERDTreeFind<CR>')
 
-vim.keymap.set('n', '<C-n>', function()
+vim.keymap.set('n', '<A-h>', '<C-w>h')                  -- Set Alt+navigation keys to move between panels
+vim.keymap.set('n', '<A-j>', '<C-w>j')
+vim.keymap.set('n', '<A-k>', '<C-w>k')
+vim.keymap.set('n', '<A-l>', '<C-w>l')
+
+vim.keymap.set('n', '<C-n>', function()                 -- Set Ctrl+n shortcut to open terminal in a new panel
   vim.cmd('split term://zsh | resize 10')
 end, {desc = 'Open terminal in new tab'})
 
