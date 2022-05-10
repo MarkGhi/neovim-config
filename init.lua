ccb45d4f7b205e516a173ee8c9a554476a53a9d6
MarcoGhi <marcoghigo98@gmail.com>
2022-05-10 00:47:56 +0000

    Added startup screen with project support

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 local Plug = vim.fn['plug#']
 
+-- Plug plugin management
 vim.call('plug#begin')
     -- Appearance
     Plug 'vim-airline/vim-airline'                      -- Fancy bottom statusbar
@@ -19,9 +20,67 @@ vim.call('plug#begin')
 
     -- Git
     Plug 'airblade/vim-gitgutter'                       -- Shows git changes in open files
+    Plug 'tpope/vim-fugitive'                           -- Git integration
 
 vim.call('plug#end')
 
+-- Packer plugin management
+require('packer').startup(function()
+    use {
+        'goolord/alpha-nvim',                           -- Startup screen
+        requires = { 'kyazdani42/nvim-web-devicons' },
+        config = function ()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.dashboard")
+            
+            -- Set header
+            dashboard.section.header.val = {
+                "                                                     ",
+                "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+                "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+                "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+                "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+                "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+                "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+                "                                                     ",
+            }
+            
+            -- Set menu
+            dashboard.section.buttons.val = {
+                dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+                dashboard.button( "f", "  > Find file", ":cd $HOME/Documents | Telescope find_files<CR>"),
+                dashboard.button( "r", "  > Recent"   , ":Telescope project<CR>"),
+                dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+                dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+            }
+
+            table.insert(dashboard.config.layout, { type = "padding", val = 1 })
+            table.insert(dashboard.config.layout, {
+              type = "text",
+              val = require "alpha.fortune"(),
+              opts = {
+                position = "center",
+                hl = "AlphaQuote",
+              },
+            }) 
+
+            -- Send config to alpha
+            alpha.setup(dashboard.opts)
+        end
+    }
+    use {
+      'nvim-telescope/telescope.nvim',
+      requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {
+      'nvim-telescope/telescope-project.nvim',
+      config = function ()
+          require('telescope').load_extension('project')
+      end
+    }
+end
+)
+     
 --Options
 vim.opt.background='dark'                               -- Apply the color set for dark colors (not only the background as it might look)
 vim.opt.clipboard='unnamedplus'                         -- Enable clipboard between Neovim and other applications
@@ -60,9 +119,10 @@ end, {desc = 'Open terminal in new tab'})
 vim.cmd[[
   syntax enable
   colorscheme gruvbox
-  autocmd VimEnter * NERDTree | wincmd p
   au TermOpen * tnoremap <Esc> <C-\><C-n>
   au BufEnter * if &buftype == 'terminal' | :startinsert | endif
 ]]
+
+--autocmd VimEnter * NERDTree | wincmd p
 
 --vim.opt. t_Co=256 #Enable 256 colors on the terminal (don't really know what it means, so off for now)
