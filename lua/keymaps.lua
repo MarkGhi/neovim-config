8032358b85a51f9c85b934f7cfcec93130dcfa64
MarcoGhi <marcoghigo98@gmail.com>
2022-05-25 15:41:33 +0000

    Added some keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,8 +1,13 @@
 vim.keymap.set("n", "<C-p>", ":NERDTreeFocus<CR>")
-vim.keymap.set("n", "<C-b>", ":NERDTreeToggle<CR>")
+vim.keymap.set("n", "<C-n>", ":NERDTreeToggle<CR>")
 vim.keymap.set("n", "<C-f>", ":NERDTreeFind<CR>")
 
-vim.keymap.set("n", "<A-h>", "<C-w>h") -- Set Alt+navigation keys to move between panels
+-- Go to beginning or end
+vim.keymap.set("n", "<C-b>", "<ESC>")
+vim.keymap.set("n", "<C-e>", "<End>")
+
+-- Set Alt+navigation keys to move between panels
+vim.keymap.set("n", "<A-h>", "<C-w>h")
 vim.keymap.set("n", "<A-j>", "<C-w>j")
 vim.keymap.set("n", "<A-k>", "<C-w>k")
 vim.keymap.set("n", "<A-l>", "<C-w>l")
@@ -11,12 +16,17 @@ vim.keymap.set("n", "<A-Down>", "<C-w>j")
 vim.keymap.set("n", "<A-Up>", "<C-w>k")
 vim.keymap.set("n", "<A-Right>", "<C-w>l")
 
-vim.keymap.set("n", "<Leader>s", ":vsplit<CR>") -- Simple vertical split shortcut
+vim.keymap.set("n", "<C-c>", "<cmd> %y+ <CR>") -- Copy all
+vim.keymap.set("n", "<C-s>", "<cmd> w <CR>") -- Save
+vim.keymap.set("n", "<C-w>", ":b#|bd# <CR>") -- Close buffer
 
-vim.keymap.set("n", "<A-]>", ":bn<CR>") -- Navigate to next/previous buffer
-vim.keymap.set("n", "<A-[>", ":bp<CR>")
+vim.keymap.set("n", "<Leader>s", ":vsplit<CR>")
 
-vim.keymap.set("n", "<C-n>", function() -- Set Ctrl+n shortcut to open terminal in a new panel
+-- Navigate to next/previous buffer
+vim.keymap.set("n", "<Tab>", ":bn<CR>")
+vim.keymap.set("n", "<S-Tab>", ":bp<CR>")
+
+vim.keymap.set("n", "<Leader>t", function() -- Set Ctrl+n shortcut to open terminal in a new panel
 	vim.cmd("split term://zsh | resize 10")
 end, { desc = "Open terminal in new tab" })
 
